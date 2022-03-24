1969489d88a627def9301ab25cd8b8b29dd7debc
subhamsatapathy <subhamsatapathy@localhost>
2022-03-24 14:05:38 +0530

    user2 update

--- src/test/java/selenium_mvn/PushThis.java
+++ src/test/java/selenium_mvn/PushThis.java
@@ -4,5 +4,7 @@ public class PushThis {
 
 	public static void main(String[] args) {
 		 System.out.println("this is to push");
+		 System.err.println("this is changed");
+		 System.out.println("user2 updated");
 	}
 }
